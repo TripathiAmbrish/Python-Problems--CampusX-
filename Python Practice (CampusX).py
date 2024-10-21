bff3855e4f4579d5e617473f09606df47851a787
TripathiAmbrish <139352292+TripathiAmbrish@users.noreply.github.com>
2024-10-21 20:11:06 +0530

    ipynb file

--- Python Practice (CampusX).py
+++ Python Practice (CampusX).py
@@ -3,7 +3,7 @@
 
 # Solution1:
 
-# In[ ]:
+# In[1]:
 
 
 # Take input from the user
@@ -24,18 +24,19 @@ print("The oldest age is:", oldest)
 
 # solution2:
 
-# In[ ]:
+# In[2]:
 
 
 #let C be celcius
 C = int(input('Enter celcius value: '))
 F = (C*(9/5)) + 32
+F = round(F, 2)
 print(F, 'F')
 
 
 # solution3:
 
-# In[ ]:
+# In[3]:
 
 
 x = int(input('Enter first no.: '))
@@ -46,7 +47,7 @@ print(y, x)
 
 # solution4:
 
-# In[ ]:
+# In[4]:
 
 
 a = int(input('1st digit: '))
@@ -56,10 +57,46 @@ c = int(input('3rd digit: '))
 print('sum of three digits is ', a+b+c)
 
 
-# In[ ]:
+# solutiob5:
+
+# In[5]:
 
 
+four_dig_num = int(input('Enter a four digit nunber: '))
 
+reversed_num = int(str(four_dig_num)[::-1])
+print(reversed_num)
+
+if four_dig_num != reversed_num:
+    print(True)
+else:
+    print(False)
+
+
+# solution6:
+
+# In[6]:
+
+
+num = int(input('Enter a number: '))
+
+if num % 2 == 0:
+    print(num,' is even')
+else:
+    print(num,' is odd')
+
+
+# solution7:
+
+# In[9]:
+
+
+year = int(input('Enter a year: '))
+
+if year / 4 and not year / 100:
+    print(year, ' is a leap year.')
+else:
+    print(year, ' not a leap year.')
 
 
 # In[ ]:
